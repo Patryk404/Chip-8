1e68e502e2359e40b284dd5e439ef73e365ec993
Patryk404 <patryk744@op.pl>
2020-08-19 23:24:38 +0200

    Displaying and basic logic

--- chip8.h
+++ chip8.h
@@ -1,12 +1,27 @@
 #pragma once
 #include <string.h>
 #include <iostream>
+#include <SFML/Graphics.hpp>
 class chip8 {
+private:
+	unsigned short opcode;
+	unsigned char memory[4096]; // the chip 8 has 4k memory
+	unsigned char V[16]; //CPU REGISTERS
+	unsigned short I; //index register
+	unsigned short pc; // program computer pc
+	unsigned char gfx[64 * 32]; // screen 
+	unsigned char delay_timer;
+	unsigned char sound_timer;
+	unsigned short stack[16];
+	unsigned short sp; // stack pointer
+	unsigned char key[16]; // 0x0-0xF
 public: 
 	chip8();
 	void initialize();//  Initialize the Chip8 system and load the game into the memory 
-	void loadGame(std::string name_game); //loading a game into the memory
+	bool loadGame(const char * name_game); //loading a game into the memory
 	void emulateCycle();// emulate one cycle of game
 	void setKeys();// store key press state (Press and Release)
+	void debugRender();
+	void render(sf::RenderWindow& window);
 	bool drawFlag;
 };
--- emulator.cpp
+++ emulator.cpp
@@ -1,20 +1,38 @@
 ﻿#define emulator_chip8 
-
+#include "chip8.h"
 #include <iostream>
+#include <conio.h>
+#include <SFML/Graphics.hpp>
 
-unsigned short opcode;
-unsigned char memory[4096]; // the chip 8 has 4k memory
-unsigned char V[16];
-unsigned short I;
-unsigned short pc;
-unsigned char gfx[64 * 32]; // screen 
-unsigned char delay_timer;
-unsigned char sound_timer;
-unsigned short stack[16];
-unsigned short sp; // stack pointer
-unsigned char key[16]; // 0x0-0xF
+chip8 CHIP;
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World!\n";
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Emulator chip-8");
+    window.setFramerateLimit(60);
+	if (argc < 2)
+	{
+		printf("Usage: myChip8.exe chip8application\n\n");
+		return 1;
+	}
+
+    CHIP.loadGame(argv[1]);
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        CHIP.emulateCycle();
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+
+        CHIP.render(window);
+
+        window.display();
+    }
 }
 
